b8728eb65e6df4f4b41599ee408d9bac8ac95a64
Alan Kong <49286489+s2131335@users.noreply.github.com>
2023-02-25 00:19:52 +0800

    ALL role added to auth

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,15 +1,16 @@
-module.exports.checkAuth = function checkAuth(permission){
-return (req,res,next)=> {
-    
-    if (!req.isAuthenticated()) 
-        return res.status(403).send("Need to login");
-    
-    if (permission && !(req.user.role.includes(permission)))
-        return res.status(403).send("Not permitted");
-    
-    return next();
-}};
+module.exports.checkAuth = function checkAuth(permission) {
+	return (req, res, next) => {
+		if (!req.isAuthenticated()) return res.status(403).send("Need to login");
+
+		if (permission && !req.user.role.includes(permission))
+			return res.status(403).send("Not permitted");
+
+		return next();
+	};
+};
 
 module.exports.ADMIN = "ADMIN";
 
-module.exports.TUTOR = "TUTOR";
+module.exports.STUDENT = "STUDENT";
+
+module.exports.ALL = "ALL";

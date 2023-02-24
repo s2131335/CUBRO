b0686f2a8e73952e25573402d3195c97bc7b451a
Alan Kong <49286489+s2131335@users.noreply.github.com>
2023-02-25 00:19:03 +0800

    testing controller typo fix

--- server/controllers/testing.js
+++ server/controllers/testing.js
@@ -1,23 +1,21 @@
-const utils = require('../utils/utils');
-const userService = require('../services/users');
+const utils = require("../utils/utils");
+const userService = require("../services/users");
 
-module.exports.generateTutor = async function generateTutor(req,res){
-    for(let i = 0; i < Math.min(100, parseInt(req.params.no)); i++)
-    {
-        let tutor = {
-            "contactNumber":utils.getRandomInt(10000000,99999999),
-            "password":"123456789",
-            "email":`${utils.getRandomInt(10000000,99999999)}@gmail.com`,
-            "fullName":`DummyTutor${utils.getRandomInt(1,99999999)}`
-        };
-        try
-        {
-            await userService.addUser(tutor);
-        }
-        catch (err)
-        {
-            return res.send(err);
-        }
-    }
-    return res.send(`successfully generated ${Math.min(100, parseInt(req.params.no))} tutors`);
-}
+module.exports.generateStudent = async function generateStudent(req, res) {
+	for (let i = 0; i < Math.min(100, parseInt(req.params.no)); i++) {
+		let tutor = {
+			contactNumber: utils.getRandomInt(10000000, 99999999),
+			password: "123456789",
+			email: `${utils.getRandomInt(10000000, 99999999)}@gmail.com`,
+			fullName: `DummyStudent${utils.getRandomInt(1, 99999999)}`,
+		};
+		try {
+			await userService.addUser(tutor);
+		} catch (err) {
+			return res.send(err);
+		}
+	}
+	return res.send(
+		`successfully generated ${Math.min(100, parseInt(req.params.no))} students`
+	);
+};

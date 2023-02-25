2c7d6985553336d5756c4942f6009525eb8569d3
Alan Kong <49286489+s2131335@users.noreply.github.com>
2023-02-25 13:21:54 +0800

    userSchema update

--- server/database/models/users.js
+++ server/database/models/users.js
@@ -1,43 +1,41 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    fullName: {
-        type: String,
-        required: true
-    },
-    nickname:{
-        type:String
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String
-    },
-    role: {
-        type: Array,
-        of: String,
-        default: ["TUTOR"]
-    },
-    skills:{
-        type:Array,
-        of: String,
-        default:[]
-    },
-    createTime: {
-        type: Date,
-        default:new Date()
-    },
-    avatar: {
-        type: String
-    },
-    contactNumber: {
-        type: Number,
-        required:true
-    }
-}, { strict: false })
+const userSchema = new Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+		},
+		nickname: {
+			type: String,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+		},
+		role: {
+			type: Array,
+			of: String,
+			default: ["Student"],
+		},
+		createTime: {
+			type: Date,
+			default: new Date(),
+		},
+		avatar: {
+			type: String,
+		},
+		contactNumber: {
+			type: Number,
+			required: true,
+		},
+	},
+	{ strict: false }
+);
 
-module.exports = mongoose.model('user', userSchema)
+module.exports = mongoose.model("user", userSchema);

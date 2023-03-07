da750a7e599e30d5b7d9950899e3fe570e2f8876
Boris Leung <borisleung2013@gmail.com>
2023-03-07 12:58:50 +0800

    Added course browse api

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -1,26 +1,46 @@
 const { parseExcel } = require("../utils/excel");
 const courseService = require("../services/courses");
+const { tutorial, lecture, course } = require("../database/models/courses");
 
 module.exports.importCourse = function importCourse(req, res) {
-	let filename = req.file.originalname;
-	let courses = parseExcel(filename);
-	if (courses.length === 0) return res.status(200).send("No update needed");
+    let filename = req.file.originalname;
+    let courses = parseExcel(filename);
+    if (courses.length === 0) return res.status(200).send("No update needed");
 
-	let failed = [];
-	for (let course of courses) {
-		let type = course.type;
-		delete course.type;
-		try {
-			courseService.addLesson(course, type);
-		} catch (err) {
-			failed.push(course.courseCode + type);
-			continue;
-		}
-	}
-	if (!(failed.length === 0)) {
-		if (failed.length === course.length)
-			return res.status(500).send("All failed");
-		else return res.status(500).send(`failed:${failed}`);
-	}
-	res.status(200).send("All successful");
+    let failed = [];
+    for (let course of courses) {
+        let type = course.type;
+        delete course.type;
+        try {
+            courseService.addLesson(course, type);
+        } catch (err) {
+            failed.push(course.courseCode + type);
+            continue;
+        }
+    }
+    if (!(failed.length === 0)) {
+        if (failed.length === course.length)
+            return res.status(500).send("All failed");
+        else return res.status(500).send(`failed:${failed}`);
+    }
+    res.status(200).send("All successful");
+};
+
+module.exports.browseCourse = async function browseCourse(req, res) {
+    try {
+        const { courseCode, courseName } = req.query;
+        var filter = {};
+        if (courseCode) {
+            filter["courseCode"] = { $regex: ".*" + courseCode + ".*" };
+        }
+        if (courseName) {
+            filter["courseName"] = { $regex: ".*" + courseName + ".*" };
+        }
+        console.log(filter);
+        var result = await course.find(filter);
+        res.status(200).json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(err.status).send(err);
+    }
 };
--- server/routes/courses.js
+++ server/routes/courses.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const {} = require("../controllers/courses");
+const { browseCourse } = require("../controllers/courses");
 const upload = require("../middleware/upload");
 const { importCourse } = require("../controllers/courses");
 const { tutorial, lecture } = require("../database/models/courses");
+const Auth = require("../middleware/auth");
 
 // router.post("/add");
 
 router.post("/import-courses", upload, importCourse);
+
+router.get("/browse", Auth.checkAuth(), browseCourse);
 
 // router.get("/excel");
 

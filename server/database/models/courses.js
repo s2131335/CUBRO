1a9bf846c10ebab470e771ea18981adb9ecf3f3b
Alan Kong <49286489+s2131335@users.noreply.github.com>
2023-03-07 22:24:48 +0800

    refractor course-api : service added

--- server/database/models/courses.js
+++ server/database/models/courses.js
@@ -2,52 +2,52 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const courseSchema = new Schema({
-    courseCode: {
-        type: String,
-    },
-    courseName: {
-        type: String,
-    },
-    venue: {
-        type: String,
-    },
-    class: {
-        // class number
-        type: Number,
-    },
-    meetings: [
-        {
-            courseCode: { type: String },
-            day: {
-                type: Number,
-            },
-            start: { type: String },
-            end: { type: String },
-            dates: {
-                type: Array,
-                of: String,
-                default: [],
-            },
-        },
-    ],
+	courseCode: {
+		type: String,
+	},
+	courseName: {
+		type: String,
+	},
+	venue: {
+		type: String,
+	},
+	class: {
+		// class number
+		type: Number,
+	},
+	meetings: [
+		{
+			courseCode: { type: String },
+			day: {
+				type: Number,
+			},
+			start: { type: String },
+			end: { type: String },
+			dates: {
+				type: Array,
+				of: String,
+				default: [],
+			},
+		},
+	],
 });
 
-const course = mongoose.model("course", courseSchema);
-const tutorial = course.discriminator("tutorial", new mongoose.Schema({}));
-const lecture = course.discriminator(
-    "lecture",
-    new mongoose.Schema({
-        instructor: {
-            type: String,
-        },
-        description: {
-            type: String,
-        },
-        seat: {
-            // available seat
-            type: Number,
-        },
-    })
+const Course = mongoose.model("course", courseSchema);
+const Tutorial = Course.discriminator("tutorial", new mongoose.Schema({}));
+const Lecture = Course.discriminator(
+	"lecture",
+	new mongoose.Schema({
+		instructor: {
+			type: String,
+		},
+		description: {
+			type: String,
+		},
+		seat: {
+			// available seat
+			type: Number,
+		},
+	})
 );
 
-module.exports = { tutorial, lecture, course };
+module.exports = { Tutorial, Lecture, Course };
--- server/services/courses.js
+++ server/services/courses.js
@@ -1,13 +1,55 @@
-const { tutorial, lecture } = require("../database/models/courses");
+const { Tutorial, Lecture, Course } = require("../database/models/courses");
 const error = require("../utils/errors");
 
 module.exports.addLesson = async function addLesson(lesson, type) {
 	try {
 		if (type === "T") {
-			await tutorial.create(lesson);
-		} else await lecture.create(lesson);
+			await Tutorial.create(lesson);
+		} else await Lecture.create(lesson);
 		return null;
 	} catch (err) {
+		throw error.DatabaseUpdate;
+	}
+};
+
+module.exports.findAllCoursesByFilter = async function (filter = {}) {
+	try {
+		let courses = await Course.find(filter);
+		return courses;
+	} catch (err) {
+		console.log("🚀 ~ file: courses.js:20 ~ err:", err);
+		return null;
+	}
+};
+
+module.exports.findCourseByFilter = async function (fields) {
+	try {
+		let c = await Course.findOne(fields);
+		return c;
+	} catch (err) {
+		console.log("🚀 ~ file: courses.js:30 ~ err:", err);
+		return null;
+	}
+};
+
+module.exports.findCourseAndUpdate = async function (filter, update) {
+	try {
+		// console.log(update);
+		await Course.findOneAndUpdate(filter, update);
+		return null;
+	} catch (err) {
+		console.log("🚀 ~ file: courses.js:41 ~ err:", err);
+		throw error.DatabaseUpdate;
+	}
+};
+
+module.exports.deleteCoursesByFilter = async function (filter) {
+	try {
+		// console.log(update);
+		await Course.deleteMany(filter);
+		return null;
+	} catch (err) {
+		console.log("🚀 ~ file: courses.js:52 ~ err:", err);
 		throw error.DatabaseUpdate;
 	}
 };

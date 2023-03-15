4bee4d04f6c5f43bee91bed6e97c853d1fd9f56d
Snorlaxusesleep <edisonlamkwokkin@gmail.com>
2023-03-15 15:59:03 +0800

    update timetable

--- frontend-server/routes/timetable.js
+++ frontend-server/routes/timetable.js
@@ -1,20 +1,23 @@
 var express = require("express");
 var router = express.Router();
-var course = [
-	{
-		courseCode: "CSCI1234",
-		venue : "LSKLSKSLSKSKL",
-		class : 1,
-		day : 1,
-		start :"1230",
-		end :"1315"
 
-		
-	}
-];
 /* GET timetable test page. */
 router.get("/", function (req, res, next) {
-	res.render("timetable/timetable", { title: "Timetable" });
+	const course = [
+		{
+			courseCode: "CSCI1234",
+			venue: "LSKLSKSLSKSKL",
+			class: 1,
+			day: 1,
+			start: "0830",
+			end: "1015",
+			ts: [0, 7],
+		},
+	];
+	res.render("timetable/timetable", {
+		title: "Timetable",
+		course,
+	});
 });
 
 module.exports = router;
